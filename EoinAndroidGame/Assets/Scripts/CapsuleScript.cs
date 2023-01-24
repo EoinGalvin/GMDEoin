f8413eb56b0af6bec2498691a3bf206a7535890f
STUDENT Eoin J. Galvin <Eoin.J.Galvin@students.ittralee.ie>
2023-01-24 10:51:35 +0000

    Signed-off-by: STUDENT Eoin J. Galvin <Eoin.J.Galvin@students.ittralee.ie>

--- EoinAndroidGame/Assets/Scripts/CapsuleScript.cs
+++ EoinAndroidGame/Assets/Scripts/CapsuleScript.cs
@@ -2,23 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SphereScript : MonoBehaviour, ITouchables
+public class CapsuleScript : MonoBehaviour, ITouchables
 {
-   
+
     // Start is called before the first frame update
     void Start()
-    {
-        
+    { 
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {     
     }
     public void OnTap()
     {
-        GetComponent<Renderer>().material.color = Color.blue;
+        GetComponent<Renderer>().material.color = Color.red;
     }
 
     public void onDrag(Ray collidingRay)
--- EoinAndroidGame/Assets/Scripts/touchManager.cs
+++ EoinAndroidGame/Assets/Scripts/touchManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class touchManager : MonoBehaviour
 {
+    private float timer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,31 +17,84 @@ public class touchManager : MonoBehaviour
     {
         if (Input.touchCount > 0)
         {
-            //acceleration += 15 * Vector3.up;
-
             Touch[] currentTouches = Input.touches;
             Touch firstTouch = currentTouches[0];
 
+            timer += Time.deltaTime;
+            switch (firstTouch.phase)
+            {
+                case TouchPhase.Began:
+                    timer = 0;
+                    break;
+                case TouchPhase.Ended:
+                    if(timer <= 0.25){
+                        touching(firstTouch);
+                    }
+                    else
+                    {
+                        //not a tap
+                    }
+                    break;
+                case TouchPhase.Moved:
+                    print("touch moved so not a touch");
+                    break;
+                case TouchPhase.Stationary:
+                    break;
+                case TouchPhase.Canceled:
+                    break;
+            }
+
+            /*
             print(firstTouch.position);
 
             Ray ray = Camera.main.ScreenPointToRay(firstTouch.position);
             Debug.DrawRay(ray.origin, 60 * ray.direction);
 
             RaycastHit hitInfo;
-            if(Physics.Raycast(ray,out hitInfo)){
-                //hitInfo.collider.gameObject.transform.position += Vector3.up;
-                //hitInfo.collider.gameObject.GetComponent<Renderer>().material.color = Color.green;
+            if (Physics.Raycast(ray, out hitInfo)) {
 
-                CubeScript touchCube = hitInfo.transform.GetComponent<CubeScript>();
-                touchCube.moveUp();
-                touchCube.changeColour(Color.green);
+                //CubeScript touchCube = hitInfo.transform.GetComponent<CubeScript>();
+                //touchCube.moveUp();
+                //touchCube.ChangeColour(Color.green);
+
+                ITouchables touchedObject = hitInfo.transform.GetComponent<ITouchables>();
+                if (touchedObject != null)
+                {
+                    touchedObject.OnTap();
+                }
+                if(touchedObject is CubeScript)
+                {
+                    (touchedObject as CubeScript).cubeOnly();
+                }
+                
+            } */
+
+            
+        }
+    }
+    public void touching(Touch firstTouch)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(firstTouch.position);
+        Debug.DrawRay(ray.origin, 60 * ray.direction);
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+
+            //CubeScript touchCube = hitInfo.transform.GetComponent<CubeScript>();
+            //touchCube.moveUp();
+            //touchCube.ChangeColour(Color.green);
+
+            ITouchables touchedObject = hitInfo.transform.GetComponent<ITouchables>();
+            if (touchedObject != null)
+            {
+                touchedObject.OnTap();
             }
-            if (Physics.Raycast(ray)){
-                print("hit");
+            if (touchedObject is CubeScript)
+            {
+                (touchedObject as CubeScript).cubeOnly();
             }
-            else{
-                print("miss");
-            }
+
         }
     }
 }
